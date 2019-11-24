756625f60d6ebea93bcab0ee7728cca65a1b202e
Dawid Rodak <dawidrodak@yahoo.com>
2019-11-24 18:49:18 +0100

    Created some dummy data

---
+++ native-app/assets/dummyData.js
@@ -0,0 +1,14 @@
+export default dummyData = {
+    user1: {
+        name: "Dawid Rodak",
+        isOnline: true,
+        lastMessage: "lubie kawke taka dobra",
+        avatar: require("../assets/avatar1.png")
+    },
+    user2: {
+        name: "Kamil Twardowski",
+        isOnline: false,
+        lastMessage: "Wysłano zdjęcie",
+        avatar: require("../assets/avatar2.png")
+    }
+};
