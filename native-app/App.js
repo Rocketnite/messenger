b3a64320634e527da9be69fdb099a8869a5758d8
Dawid Rodak <dawidrodak@yahoo.com>
2019-11-24 22:48:34 +0100

    Created HomeScreen and basic file structure

--- native-app/App.js
+++ native-app/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    const [currentScreen, setCurrentScreen] = useState("home");
+
+    const context = <HomeScreen />;
+
+    return <View style={styles.container}>{context}</View>;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
 });
---
+++ native-app/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const HomeScreen = props => {
+    return (
+        <View>
+            <Text> Hello World </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
