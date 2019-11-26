4fb8442385d8299671b2b1c9f1d4df6b5734f691
Dawid Rodak <dawidrodak@yahoo.com>
2019-11-27 00:17:09 +0100

    Henio from Tesco commercials added to dummyData

--- native-app/assets/dummyData.js
+++ native-app/assets/dummyData.js
@@ -13,5 +13,68 @@ export default dummyData = [
         isOnline: false,
         lastMessage: "Ja: Wysłano zdjęcie",
         avatar: require("../assets/avatar2.png")
+    },
+    {
+        id: 3,
+        name: "Henio",
+        isOnline: false,
+        lastMessage: "Henio: Proszę pomóż mi",
+        avatar: require("../assets/henio.jpeg")
+    },
+    {
+        id: 4,
+        name: "Henio",
+        isOnline: false,
+        lastMessage: "Henio: Widzę tylko ciemność",
+        avatar: require("../assets/henio.jpeg")
+    },
+    {
+        id: 5,
+        name: "Henio",
+        isOnline: false,
+        lastMessage: "Henio: Widzę tylko ciemność",
+        avatar: require("../assets/henio.jpeg")
+    },
+    {
+        id: 6,
+        name: "Henio",
+        isOnline: false,
+        lastMessage: "Henio: Widzę tylko ciemność",
+        avatar: require("../assets/henio.jpeg")
+    },
+    {
+        id: 7,
+        name: "Henio",
+        isOnline: false,
+        lastMessage: "Henio: Widzę tylko ciemność",
+        avatar: require("../assets/henio.jpeg")
+    },
+    {
+        id: 8,
+        name: "Henio",
+        isOnline: false,
+        lastMessage: "Henio: Widzę tylko ciemność",
+        avatar: require("../assets/henio.jpeg")
+    },
+    {
+        id: 9,
+        name: "Henio",
+        isOnline: false,
+        lastMessage: "Henio: Widzę tylko ciemność",
+        avatar: require("../assets/henio.jpeg")
+    },
+    {
+        id: 10,
+        name: "Henio",
+        isOnline: false,
+        lastMessage: "Henio: Widzę tylko ciemność",
+        avatar: require("../assets/henio.jpeg")
+    },
+    {
+        id: 11,
+        name: "Henio",
+        isOnline: false,
+        lastMessage: "Henio: Widzę tylko ciemność",
+        avatar: require("../assets/henio.jpeg")
     }
 ];
