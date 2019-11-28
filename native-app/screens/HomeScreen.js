c87ac53bd08cc662de8e8a33da0be554b349e795
Dawid Rodak <dawidrodak@yahoo.com>
2019-11-28 23:38:38 +0100

    Added title and search icon

--- native-app/screens/HomeScreen.js
+++ native-app/screens/HomeScreen.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, Text } from "react-native";
 import ChatSelection from "../components/ChatSelection";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const HomeScreen = props => {
     return (
         <View style={styles.container}>
             <ScrollView style={styles.chatList}>
+                <View style={styles.searchContainer}>
+                    <MaterialIcons name="search" size={28} />
+                </View>
+                <Text style={styles.title}> Messages </Text>
                 {props.data.map(user => (
                     <ChatSelection
                         key={user.id}
@@ -24,6 +29,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginVertical: 40
+    },
+
+    searchContainer: {
+        alignItems: "flex-end",
+        marginRight: 20
+    },
+
+    title: {
+        fontSize: 32,
+        marginHorizontal: 11,
+        marginBottom: 11,
+        fontWeight: "500",
+        color: "#2b385a"
     },
 
     chatList: {}
