6cad04d9918833c277e876e78e6445a133b6bb67
rodakd <dawidrodak@yahoo.com>
2019-12-25 19:40:10 +0100

    Change View to SafeAreaView in HomeScreen

--- native-app/components/SearchBar.js
+++ native-app/components/SearchBar.js
@@ -9,20 +9,12 @@ const isBigScreen = Dimensions.get('window').height > dims.bigScreenHeight;
 const SearchBar = () => {
   const iconSize = isBigScreen ? 44 : 22;
   return (
-    <View animation="slideInRight" duration={1000} style={styles.container}>
+    <View animation="slideInRight" duration={1000}>
       <TouchableOpacity>
         <Feather name="search" size={iconSize} />
       </TouchableOpacity>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    marginRight: isBigScreen ? 60 : 15
-  }
-});
 
 export default SearchBar;
--- native-app/screens/HomeScreen.js
+++ native-app/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, StyleSheet, ScrollView, Text, Dimensions } from 'react-native';
 import * as Animated from 'react-native-animatable';
 import PropTypes from 'prop-types';
+import { SafeAreaView } from 'react-navigation';
+
 import TextChannel from '../components/TextChannel';
 import SearchBar from '../components/SearchBar';
 import NavigationTabs from '../components/NavigationTabs';
@@ -12,10 +14,12 @@ const isBigScreen = Dimensions.get('window').height > dims.bigScreenHeight;
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <SearchBar />
+    <SafeAreaView style={styles.container} forceInset={{ top: 'always' }}>
+      <View style={styles.header}>
         <Text style={styles.title}> Messages </Text>
+        <SearchBar />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <Animated.View animation="slideInUp" duration={1000}>
           {data.map(user => (
             <TextChannel navigation={navigation} key={user.id} user={user} />
@@ -23,15 +27,22 @@ const HomeScreen = ({ navigation }) => {
         </Animated.View>
       </ScrollView>
       <NavigationTabs />
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 40,
     alignItems: 'center'
+  },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '95%',
+    alignItems: 'center',
+    padding: 5
   },
 
   searchContainer: {
@@ -41,8 +52,6 @@ const styles = StyleSheet.create({
 
   title: {
     fontSize: isBigScreen ? 64 : 32,
-    marginHorizontal: 11,
-    marginBottom: 11,
     fontFamily: 'rubik-bold',
     color: '#2b385a'
   }
