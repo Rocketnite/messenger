52dc24f8e8efc19e94ee271b4f3305e3b9cd7451
rodakd <dawidrodak@yahoo.com>
2019-12-25 20:00:15 +0100

    Use more performant FlatList instead of ScrollView

--- native-app/screens/HomeScreen.js
+++ native-app/screens/HomeScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView, Text, Dimensions } from 'react-native';
+import { View, StyleSheet, FlatList, Text, Dimensions } from 'react-native';
 import * as Animated from 'react-native-animatable';
 import PropTypes from 'prop-types';
 import { SafeAreaView } from 'react-navigation';
 
+import { render } from 'react-dom';
 import TextChannel from '../components/TextChannel';
 import SearchBar from '../components/SearchBar';
 import NavigationTabs from '../components/NavigationTabs';
@@ -13,19 +14,23 @@ import data from '../assets/dummyData';
 const isBigScreen = Dimensions.get('window').height > dims.bigScreenHeight;
 
 const HomeScreen = ({ navigation }) => {
+  const renderItem = user => (
+    <TextChannel navigation={navigation} user={user.item} />
+  );
+
   return (
     <SafeAreaView style={styles.container} forceInset={{ top: 'always' }}>
       <View style={styles.header}>
         <Text style={styles.title}> Messages </Text>
         <SearchBar />
       </View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <Animated.View animation="slideInUp" duration={1000}>
-          {data.map(user => (
-            <TextChannel navigation={navigation} key={user.id} user={user} />
-          ))}
-        </Animated.View>
-      </ScrollView>
+      <Animated.View animation="slideInUp" duration={2000}>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </Animated.View>
       <NavigationTabs />
     </SafeAreaView>
   );
