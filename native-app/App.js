d80afdb86389a3575477bc9ef6d3d80a36cb73c7
rodakd <dawidrodak@yahoo.com>
2019-12-26 10:56:40 +0100

    Refactored font loader

--- native-app/App.js
+++ native-app/App.js
@@ -3,24 +3,24 @@ import { AppLoading } from 'expo';
 import * as Fonts from 'expo-font';
 import ScreenNavigator from './navigation/ScreenNavigator';
 
-export default function App() {
-  const [isReady, setIsReady] = useState(false);
+const fetchFonts = () => {
+  return Fonts.loadAsync({
+    'rubik-bold': require('./assets/fonts/Rubik-Bold.ttf'),
+    'rubik-light': require('./assets/fonts/Rubik-Light.ttf'),
+    'rubik-italic': require('./assets/fonts/Rubik-Italic.ttf'),
+    'rubik-regular': require('./assets/fonts/Rubik-Regular.ttf'),
+    'rubik-medium': require('./assets/fonts/Rubik-Medium.ttf')
+  });
+};
 
-  if (!isReady) {
-    console.log('Ready');
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
     return (
       <AppLoading
-        startAsync={() => {
-          return Fonts.loadAsync({
-            'rubik-bold': require('./assets/fonts/Rubik-Bold.ttf'),
-            'rubik-light': require('./assets/fonts/Rubik-Light.ttf'),
-            'rubik-italic': require('./assets/fonts/Rubik-Italic.ttf'),
-            'rubik-regular': require('./assets/fonts/Rubik-Regular.ttf'),
-            'rubik-medium': require('./assets/fonts/Rubik-Medium.ttf')
-          });
-        }}
-        onFinish={() => setIsReady(true)}
-        onError={err => console.log(err)}
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
       />
     );
   }
